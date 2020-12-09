9b275d6e66b0e8d798edeafc5dd51f65e53f5a23
Paul Gomez <prego.paul25@gmail.com>
2020-12-08 19:00:46 -0500

    Iniciando con el CRUD

---
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,12 @@
+package com.platzi.market.persistence.crud;
+
+import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    List<Producto> findByIdCategoria(int idCategoria);
+
+}

777953dc5d945565f9fd48951c1212bba9cad521
Paul Gomez <prego.paul25@gmail.com>
2020-12-18 17:18:35 -0500

    Se modificó el repositorio para que este orientado al dominio y no a una tabla puntual

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -1,38 +1,56 @@
 package com.platzi.market.persistence;
 
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import com.platzi.market.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository //Indica que es la clase que se encarga de interactuar con la BD, asi tambien se puede usar @Component pero es mas generalizado
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository { //Se implementan los metodos de ProductRepository
     private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper; //se agrega el atributo ProductMapper para poder hacer la convierte el Producto a Product
 
-    public List<Producto> getAll(){
-        return (List<Producto>) productoCrudRepository.findAll();
+    @Override
+    public List<Product> getAll(){ //Aqui debe retornar una lista de Product, mas no de Productos
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll(); //Una lista de productos recuperdos de la BD
+        return mapper.toProducts(productos); //que retorne el mapper que convierte una lista de productos en products
     }
 
-    public List<Producto> getByCategoria(int idCategoria){
-        return productoCrudRepository.findByIdCategoriaOrderOrderByNombreAsc(idCategoria);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
     }
 
-    public Optional<List<Producto>> getEscasos(int cantidad){
-        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        //Al no tener un mapeador que me convierta una lista de opcionales, entonces a los productos se le realiza un map
+        //La flechita recibe los productos que tiene adentro, los convierte en products y los retorna
+        return productos.map(prods -> mapper.toProducts(prods));
     }
 
-    public Optional<Producto> getProducto (int idProducto){
-        return productoCrudRepository.findById(idProducto);
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        Optional<Producto> productos = productoCrudRepository.findById(productId);
+        return productos.map(prod -> mapper.toProduct(prod));
     }
 
-    public Producto save(Producto producto){
-        return productoCrudRepository.save(producto);
+    @Override
+    public Product save(Product product) {
+        //El save al no esperar un product, sino un producto, se le realiza la conversión inversa
+        Producto producto = mapper.toProducto(product); //Se mapea al revez para que al return le llegue el producto y no el product
+        return mapper.toProduct(productoCrudRepository.save(producto));
     }
 
-    public void delete(int idProducto){
-        productoCrudRepository.deleteById(idProducto);
+    @Override
+    public void delete(int productoId){
+        productoCrudRepository.deleteById(productoId);
     }
 
 }
